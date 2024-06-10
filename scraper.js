5b1622a426b42ac58f2be2c198aab5899014e014
Matias Bubello <matt.bubello@gmail.com>
2024-06-10 14:12:55 -0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,33 +1,56 @@
 const puppeteer = require('puppeteer');
 
+async function getPublicRepos(username) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const url = `https://github.com/${username}?tab=repositories&type=source`;
+    await page.goto(url);
+
+    await page.waitForSelector('h3');
+
+    const publicRepos = await page.evaluate(() => {
+        const repos = [];
+        const repoElements = document.querySelectorAll('h3');
+        repoElements.forEach(element => {
+            const repoName = element.textContent.trim();
+            repos.push(repoName);
+        });
+        return repos;
+    });
+
+    await browser.close();
+
+    return publicRepos;
+}
+
 async function getCommits(username, repo) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const url = `https://github.com/${username}/${repo}`;
     await page.goto(url);
 
-    // Esperar a que se cargue el contenido
     await page.waitForSelector('span.Text-sc-17v1xeu-0.gPDEWA');
 
-    // Extraer el texto del span con las clases especificadas
     const commitsText = await page.evaluate(() => {
         const span = document.querySelector('span.Text-sc-17v1xeu-0.gPDEWA');
         return span ? span.textContent.trim() : '';
     });
 
-    // Cerrar el navegador
     await browser.close();
 
-    // Convertir el texto a un número
-    const commits = parseInt(commitsText.replace(/,/g, ''), 10) || 0;
-    return commits;
+    return parseInt(commitsText.replace(/,/g, ''), 10) || 0;
 }
 
 async function main() {
-    const username = 'TuUsuarioGitHub';  // Reemplaza con tu nombre de usuario de GitHub
-    const repo = 'NombreDelRepositorio';  // Reemplaza con el nombre de tu repositorio
-    const commits = await getCommits(username, repo);
-    console.log(`Commits en ${repo}: ${commits}`);
+    const username = 'MattEzekiel';
+    const publicRepos = await getPublicRepos(username);
+    let totalCommits = 0;
+    for (const repo of publicRepos) {
+        const commits = await getCommits(username, repo);
+        console.log(`Commits en ${repo}: ${commits}`);
+        totalCommits += commits;
+    }
+    console.log(`Total de commits en todos los repositorios públicos: ${totalCommits}`);
 }
 
 main().catch(console.error);
